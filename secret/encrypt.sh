0758b9b5ea017c683c630182ad7aae3b226c4026
Mikhail Savochkin <crioto@users.noreply.github.com>
2018-06-02 11:57:33 +0600

    Minor fixes

--- secret/encrypt.sh
+++ secret/encrypt.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-os=`uname`
+oos=`uname`
 wos=${a:0:5}
+os=
 
-if [ "$os" == "Darwin" ]; then
+if [ "$oos" == "Darwin" ]; then
     echo "Encrypting on Darwin"
-elif [ "$os" == "Linux" ]; then
+    os="darwin"
+elif [ "$oos" == "Linux" ]; then
     echo "Encyrpting on Linux"
+    os="linux"
 elif [ "$wos" == "MINGW" ]; then
     echo "Encyrpting on Windows"
+    os="windows"
 else
-    echo "Unknown operating system: ${os}"
+    echo "Unknown operating system: ${oos}"
     exit 2
+fi
+
+echo "Checking required software"
+
+if [ "$os" == "windows" ]; then
+    nuget.exe
+    if [ $? -ne 0 ]; then
+        echo "nuget package wasn't found in path. Specify path to nuget.exe with --nuget-path option. See help for more info."
+        exit 3
+    fi
 fi
